3144501292e7856b18d1a929396bc95e7d9ded73
KevinC911 <00157320@uca.edu.sv>
2025-06-02 15:41:36 -0600

    adding sharp for image optimization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const sharp = require('sharp');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +18,26 @@ app.use('/auth', auth);
 app.use('/news', news);
 app.use('/banner', banner);
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the API!' });
-    });
+app.get('/serve/:filename', async (req, res) => {
+    const filename = req.params.filename;
+
+    try {
+    const imagePath = path.join(__dirname, filename);
+
+    res.set('Content-Type', 'image/webp');
+
+    const resizedImage = await sharp(imagePath)
+    .webp({ quality: 75 })
+    .toBuffer();
+
+    res.send(resizedImage);
+
+    } catch (error) {
+        console.error("Error processing image:", error);
+        res.status(404).send("Error processing image");
+    }
+
+})
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
